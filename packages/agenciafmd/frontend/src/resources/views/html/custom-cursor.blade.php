36a21ebd27eaf6499469ffbea99e1df2658514d3
vagrant <vagrant@homestead>
2020-05-28 15:56:21 +0000

    first commit

---
+++ packages/agenciafmd/frontend/src/resources/views/html/custom-cursor.blade.php
@@ -0,0 +1,7 @@
+@extends('agenciafmd/frontend::master-html')
+
+@section('content')
+<section class="cursor-content position-relative">
+  <div id="cursor" class="custom-cursor"></div>
+</section>
+@endsection
